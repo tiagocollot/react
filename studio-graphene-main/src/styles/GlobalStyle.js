7ff24a7ec67fc75491e04a95276b130da7428919
Tiago Collot <collot.tiago1@gmail.com>
2022-06-05 23:24:07 +0100

    done

--- studio-graphene-main/src/styles/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components"
-import reset from "styled-reset"
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  /* other styles */
-`
-
-export default GlobalStyle
--- studio-graphene-main/src/styles/index.js
+++
@@ -1,7 +0,0 @@
-import colors from "./colors"
-import GlobalStyle from "./GlobalStyle"
-
-export {
-  colors,
-  GlobalStyle,
-}
